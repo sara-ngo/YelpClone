25aad4824ec2adfe209ac48ef891b2a84dc4956a
Tuyet Ngo <tuyetnhi.ngo@sjsu.edu>
2022-09-21 22:14:27 -0700

    develop update function

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -1,9 +1,11 @@
 import React, { useContext, useEffect } from 'react'
 import RestaurantFinder from '../apis/RestaurantFinder'
 import { RestaurantsContext } from '../context/RestaurantsContext'
+import { useNavigate } from 'react-router-dom'
 
 const RestaurantList = (props) => {
     const { restaurants, setRestaurants } = useContext(RestaurantsContext)
+    let navigate = useNavigate()
 
     useEffect(() => {
         const fetchData = async () => {
@@ -28,6 +30,9 @@ const RestaurantList = (props) => {
         }
     }
 
+    const handleUpdate = (id) => {
+        navigate(`/restaurants/${id}/update`)
+    }
 
     return (
         <div className='list-group'>
@@ -52,7 +57,7 @@ const RestaurantList = (props) => {
                                 <td>{"$".repeat(restaurant.price_range)}</td>
                                 <td>Reviews</td>
                                 <td>
-                                    <button className='btn btn-warning'>Update</button>
+                                    <button onClick={() => handleUpdate(restaurant.id)} className='btn btn-warning'>Update</button>
                                 </td>
                                 <td>
                                     <button onClick={() => handleDelete(restaurant.id)} className='btn btn-danger'>Delete</button>
