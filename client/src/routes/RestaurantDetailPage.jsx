fdfa9017941201d7875998b08612f6bd4f3edd7e
Tuyet Ngo <tuyetnhi.ngo@sjsu.edu>
2022-10-18 20:45:05 -0700

    add reviews

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -3,6 +3,9 @@ import { useEffect } from 'react'
 import { useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import RestaurantFinder from '../apis/RestaurantFinder'
+import AddReview from '../components/AddReview'
+import Reviews from '../components/Reviews'
+import StarRating from '../components/StarRating'
 import { RestaurantsContext } from '../context/RestaurantsContext'
 
 const RestaurantDetailPage = () => {
@@ -13,7 +16,7 @@ const RestaurantDetailPage = () => {
     const fetchData = async () => {
       try {
         const response = await RestaurantFinder.get(`/${id}`)
-        setSelectedRestaurant(response.data.data.restaurants)
+        setSelectedRestaurant(response.data.data)
       } catch (err) {
         console.log(err)
       }
@@ -22,7 +25,16 @@ const RestaurantDetailPage = () => {
   }, [])
 
   return (
-    <div>{ selectedRestaurant && selectedRestaurant.name }</div>
+    <div>
+      { selectedRestaurant && (
+        <>
+          <h1 className='text-center display-1'>{selectedRestaurant.restaurants.name}</h1>
+          <div className="mt-3">
+            <Reviews reviews={selectedRestaurant.reviews} />
+          </div>
+          <AddReview />
+        </>
+    )}</div>
   )
 }
 
