7ed9c9c13b09608a5f38558cc2bc0edac074d3af
ntran12 <mia.tran124@gmail.com>
2022-11-17 15:43:36 -0800

    fix duplicated review

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -5,30 +5,31 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
 const Reviews = ({ reviews }) => {
+  const slidesToShow = 3;
+
   const settings = {
     autoplay: true,
     autoplaySpeed: 3000,
     centerMode: true,
     centerPadding: "100px",
-    slidesToShow: 3,
+    slidesToShow: slidesToShow,
     dots: true,
-    infinite: true,
+    infinite: (reviews.length > slidesToShow && true) || false,
     speed: 1000,
-    slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
         },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
         },
       },
       {
@@ -45,9 +46,9 @@ const Reviews = ({ reviews }) => {
   return (
     <div>
       <Slider {...settings}>
-        {reviews.map((review) => {
+        {reviews.map((review, i) => {
           return (
-            <div className="w-100 px-2">
+            <div key={i} className="w-100 px-2">
               <div
                 key={review.id}
                 className="card text-white bg-dark mb-3 mr-4 w-100"
